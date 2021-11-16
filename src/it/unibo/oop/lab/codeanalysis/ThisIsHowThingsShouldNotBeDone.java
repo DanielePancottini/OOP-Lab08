50ac25c933ff703f87d69a80273f235e03d74de9
Daniele Pancottini <43572647+DanielePancottini@users.noreply.github.com>
2021-11-16 09:11:52 +0100

    - complete ex1

--- src/it/unibo/oop/lab/codeanalysis/ThisIsHowThingsShouldNotBeDone.java
+++ src/it/unibo/oop/lab/codeanalysis/ThisIsHowThingsShouldNotBeDone.java
@@ -10,11 +10,8 @@ import java.util.Collection;
  * This code triggers static code analyzers. You should use it to see how things
  * SHOULD NOT be done.
  *
- * @author Danilo Pianini
- * @version 1.2
- *
  */
-public class ThisIsHowThingsShouldNotBeDone {
+public final class ThisIsHowThingsShouldNotBeDone {
 
     /*
      * PMD complains:
@@ -30,6 +27,8 @@ public class ThisIsHowThingsShouldNotBeDone {
      * 
      * This class should not have a public or default constructor
      */
+    private ThisIsHowThingsShouldNotBeDone() {
+    }
 
     /*
      * PMD complains:
@@ -43,13 +42,13 @@ public class ThisIsHowThingsShouldNotBeDone {
      * 
      * Missing Javadoc
      */
-    public static void main(String[] a) {
+    public static void main(final String[] a) {
         /*
          * PMD Complains:
          * 
          * this variable should be final!
          */
-        Collection<Object> c = new ArrayList<>();
+        final Collection<Object> c = new ArrayList<>();
         /*
          * PMD complains:
          * 
@@ -61,14 +60,16 @@ public class ThisIsHowThingsShouldNotBeDone {
          * 
          * What does it mean "43"? It's a magic number!
          */
-        for (int i = 0; i < 43; i++)
+        final int nIterations = 43;
+        for (int i = 0; i < nIterations; i++) {
             c.add(new Object());
+        }
         /*
          * FindBugs complains
          * 
          * Slow: use clear() instead!
          */
-        c.removeAll(c);
+        c.clear();
     }
 
 }
--- src/it/unibo/oop/lab/simplegui/MiniGUI.java
+++ src/it/unibo/oop/lab/simplegui/MiniGUI.java
@@ -10,9 +10,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  * This class is a simple application that writes a random number on a file.
@@ -24,6 +26,7 @@ import javax.swing.JPanel;
 public class MiniGUI {
 
     private static final String TITLE = "A very simple GUI application";
+    private static final String RESULT_TEXT_PREFIX = "Result: ";
     private static final int PROPORTION = 5;
     private final Random rng = new Random();
     private final JFrame frame = new JFrame(TITLE);
@@ -35,16 +38,30 @@ public class MiniGUI {
         final JPanel canvas = new JPanel();
         canvas.setLayout(new BorderLayout());
         final JButton write = new JButton("Print a random number on standard output");
-        canvas.add(write, BorderLayout.CENTER);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*
+         * New Panel with horizontal Box Layout
+         * */
+        final JPanel newPanel = new JPanel();
+        newPanel.setLayout(new BoxLayout(newPanel, BoxLayout.X_AXIS));
+        newPanel.add(write);
+        /*
+         * Declare JTextFIeld to show random int into GUI
+         * */
+        final JTextField resultRandom = new JTextField(MiniGUI.RESULT_TEXT_PREFIX);
+        /*
+         * Add box layout panel to border layout panel
+         * */
+        canvas.add(newPanel, BorderLayout.CENTER);
+        canvas.add(resultRandom, BorderLayout.NORTH);
         /*
          * Handlers
          */
         write.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
-                System.out.println(rng.nextInt());
+                resultRandom.setText(MiniGUI.RESULT_TEXT_PREFIX + Integer.toString(MiniGUI.this.rng.nextInt()));
             }
         });
     }
@@ -72,6 +89,7 @@ public class MiniGUI {
          * OK, ready to pull the frame onscreen
          */
         frame.setVisible(true);
+        frame.pack();
     }
 
     /**
